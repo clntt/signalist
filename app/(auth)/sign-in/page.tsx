c1953b424bfeadf110ae0c4a749c394549361d92
clnt <c.connery14@gmail.com>
2025-10-05 11:07:38 +0100

    implement UI auth and layout updated

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -11,7 +11,7 @@ const SignIn = () => {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm<SignUpFormData>({
+  } = useForm<SignInFormData>({
     defaultValues: {
       email: "",
       password: "",
@@ -19,7 +19,7 @@ const SignIn = () => {
     mode: "onBlur",
   });
 
-  const onSubmit = async (data: SignUpFormData) => {
+  const onSubmit = async (data: SignInFormData) => {
     try {
     } catch (error) {
       console.log(error);
